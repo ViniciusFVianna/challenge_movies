c263e3191cc9b8093e6129ea9685e89da2fceb7b
ViniciusViannaP <vinicius.vianna@pasqualisolution.com.br>
2022-07-05 17:38:37 -0300

    Project configuration

---
+++ lib/app/utilities/constants_strings.dart
@@ -0,0 +1,9 @@
+class ConstantsStrings {
+  static const String baseUrl = 'http://150.230.64.79:9292';
+
+  static const String authPrefs = 'auth-prefs';
+  static const String authUserPrefs = 'auth-user-prefs';
+  static const String sellerIdPrefs = 'seller-id-prefs';
+  static const String userCep = 'user-ceps';
+
+}
